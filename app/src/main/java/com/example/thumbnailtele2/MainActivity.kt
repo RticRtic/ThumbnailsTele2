f583fd4bfd84b64e6c04ba2e4c1e6ab6fc7116c8
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-09-12 15:35:02 +0200

    movieDuration added in

--- app/src/main/java/com/example/thumbnailtele2/MainActivity.kt
+++ app/src/main/java/com/example/thumbnailtele2/MainActivity.kt
@@ -18,7 +18,7 @@ import org.xmlpull.v1.XmlPullParserException
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.StringReader
 import java.net.URL
-
+import java.time.Duration
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     var client: OkHttpClient = OkHttpClient()
     var url =
         "https://vcdn.tv.comhem.se/vod/dash/cenc/HD/25fps/high/2ch/2nd/a48ea5c9-0d09-419d-9bbf-4636ca4968da/manifest?chSessionId=5d8b6648-97c1-44c3-b182-5ea4d025f9d7"
+    //   https://vcdn.tv.comhem.se/vod/dash/cenc/HD/25fps/high/2ch/2nd/0419d03f-d48c-4b0e-8996-bd8d24fdd7b6/manifest?chSessionId=9d6f1882-4a90-499f-aaf5-cb7aab2e17ed
     var addMutableValueToUrl = 0
     var finalUrl = ""
 
@@ -37,8 +38,8 @@ class MainActivity : AppCompatActivity() {
     //Seekbar
     val seekBarMinValue = 0
     var finalMovieDuration = 0
-//    var seekBarMaxValue = 0
     val seekBarStep = 6
+
 
 
 
@@ -46,22 +47,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sbSeekBar = findViewById<SeekBar>(R.id.sbSeekBar)
-        val etEnterUrl = findViewById<EditText>(R.id.etUrlRequest)
-        val btnGetMovie = findViewById<Button>(R.id.btnGetRequest)
+
         val relativeLayout = findViewById<RelativeLayout>(R.id.relativeLayout)
 
-
+        val sbSeekBar = findViewById<SeekBar>(R.id.sbSeekBar)
         sbSeekBar.progress = seekBarMinValue
-        sbSeekBar.max = finalMovieDuration
 
-
-        btnGetMovie?.setOnClickListener {
-            fetchRequest(url)
-
-
-            Log.d("BUTTON", "BUTTON: ${finalMovieDuration} ")
-        }
 
 
         sbSeekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
@@ -74,13 +65,10 @@ class MainActivity : AppCompatActivity() {
 
                 if(progress in seekBarMinValue..finalMovieDuration){
                     addMutableValueToUrl = progress / seekBarStep
-//                   relativeLayout.setPadding(paddingLeft,0,0,0)
+                    Log.d("finalmMovieDuration", "finalmMovieDuration $finalMovieDuration")
+//                    relativeLayout.setPadding(paddingLeft,0,0,0)
+
                 }
-
-                Log.d("paddingLeft", "addMutableValueToUrl: $addMutableValueToUrl ")
-                Log.d("paddingLeft", "moveThumbNail: $moveThumbNail ")
-
-
 
             }
 
@@ -117,8 +105,6 @@ class MainActivity : AppCompatActivity() {
 
         return result
     }
-
-
 
     private fun fetchRequest(sUrl: String) {
         lifecycleScope.launch(Dispatchers.IO) {
@@ -181,8 +167,6 @@ class MainActivity : AppCompatActivity() {
                             val changedUrlWithRemovedChar = newUrl.substring(0, newUrl.indexOf("$"))
                             val addToEndUrl = ".jpg?scale=160x90&d=6000"
                             finalUrl = changedUrlWithRemovedChar + addMutableValueToUrl + addToEndUrl
-//                            Log.d("SUBSTRING", "parseXmlUsePullParser: $finalUrl ")
-
 
                             //show image
                             Picasso.with(this)
@@ -191,18 +175,16 @@ class MainActivity : AppCompatActivity() {
 
 
 
-                        // Get xml mediaPresentationDuration element text value. And modify it
+                            // Get xml mediaPresentationDuration element text value. And modify it
                         } else if ("MPD".equals(nodeName, ignoreCase = true)) {
-                            val movieDuration =
-                                xmlPullParser.getAttributeValue(null, "mediaPresentationDuration")
+                            val sbSeekBar = findViewById<SeekBar>(R.id.sbSeekBar)
+                            val movieDuration = xmlPullParser.getAttributeValue(null, "mediaPresentationDuration")
                             val changedMovieDurationValueWithFirstLetters = movieDuration.drop(2)
                             val changedMovieDurationValueWithLastLetter = changedMovieDurationValueWithFirstLetters.dropLast(1)
                             finalMovieDuration = changedMovieDurationValueWithLastLetter.toInt()
-
-
+                            sbSeekBar.max = finalMovieDuration
 
                         }
-
                     }
                 }
                 eventType = xmlPullParser.next()
@@ -214,8 +196,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
-
 
 }
 
